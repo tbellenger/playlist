6ef4fcf244cf260ca540b2f535bda81f0ef5456f
tbellenger <tbellenger@gmail.com>
2021-10-27 22:44:51 -0700

    test page

--- assets/scripts/prefix.js
+++ assets/scripts/prefix.js
@@ -3,6 +3,6 @@ var eventListArray = []; // Array of events from TicketMaster
 
 // Spotify Auth Details
 const client_id = '6a0256e60f084740acaba82df07a21e2';
-const redirect_uri = 'https://tbellenger.github.io/playlist/callback/'; // Your redirect uri
+//const redirect_uri = 'https://tbellenger.github.io/playlist/callback/'; // Your redirect uri
 // testing url
-//const redirect_uri = 'http://127.0.0.1:5500/callback/'; // Your redirect uri
+const redirect_uri = 'http://127.0.0.1:5500/callback/'; // Your redirect uri
